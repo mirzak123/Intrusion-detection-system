5480f1d4adc3e27b645399a98194399f734938ed
Mirza Karic <mirza1karic1@gmail.com>
2025-01-09 16:51:38 +0100

    Working code

--- sensor/src/intruder_detected.py
+++
@@ -1,4 +0,0 @@
-from paho.mqtt import publish
-
-publish.single("IoTFundamentals/IntrusionDetectionSystem", "Intruder detected!", hostname = "test.mosquitto.org")
-print("Done")
--- sensor/src/intruder_left.py
+++
@@ -1,4 +0,0 @@
-from paho.mqtt import publish
-
-publish.single("IoTFundamentals/IntrusionDetectionSystem", "No intruder in proximity.", hostname = "test.mosquitto.org")
-print("Done")
